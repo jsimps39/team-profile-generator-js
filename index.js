8ade27bf59b446953f4d381b0c3265269d10d248
James S <jamests1000@gmail.com>
2023-01-11 23:02:37 -0500

    fixed functions

--- index.js
+++ index.js
@@ -7,90 +7,95 @@ const Intern = require('./lib/Intern');
 const pageTemplate = require('./src/page-template');
 
 const prompt = inquirer.createPromptModule();
-
-const teamArray =[];
-
-const chooseEmployeeType = ( {type} ) => {
-    let response;
-    switch(type) {
-        case 'Engineer':{
-            response = prompt(engineerQuestions);
-        }
-        case 'Intern':{
-            response = prompt(internQuestions);
-        }
-        case 'Manager':{
-            response = prompt(managerQuestions);
-        }
-    }
-    console.log('response');
-};
+const teamArray = [];
 
 const managerQuestions = [
     {
         message: 'What is the managers name?',
-        name: 'name'
+        name: 'name',
+        default: 'John'
     },
     {
         message: 'What is the managers id?',
-        name: 'id'
+        name: 'id',
+        default: '123'
     },
     {
         message: 'What is the managers email?',
-        name: 'email'
+        name: 'email',
+        default: 'john@mail.com'
     },
     {
         message: 'What is the managers office number?',
-        name: 'officeNumber'
+        name: 'officeNumber',
+        default: '1234567890'
     }
- ];//
+ ];
+
 const engineerQuestions = [
     {
         message: 'What is the engineers name?',
-        name: 'name'
+        name: 'name',
+        default: 'Paul'
     },
     {
         message: 'What is the engineers id?',
-        name: 'id'
+        name: 'id',
+        default: '456'
     },
     {
         message: 'What is the engineers email?',
-        name: 'email'
+        name: 'email',
+        default: 'paul@mail.com'
     },
     {
         message: 'What is the engineers github?',
-        name: 'gitHub'
+        name: 'gitHub',
+        default: 'paul456'
     }
-];//.then(({ name, id, email, gitHub}) => {
-//     const engineer = new Engineer(name, id, email, gitHub);
-//     teamArray.push(engineer);
-//     console.log(teamArray);
-// });
+];
+
 const internQuestions = [
     {
         message: 'What is the interns name?',
-        name: 'name'
+        name: 'name',
+        default: 'Larry'
     },
     {
         message: 'What is the interns id?',
-        name: 'id'
+        name: 'id',
+        default: '789'
     },
     {
         message: 'What is the interns email?',
-        name: 'email'
+        name: 'email',
+        default: 'larry@mail.com'
     },
     {
         message: 'What is the interns school?',
-        name: 'school'
+        name: 'school',
+        default: 'Harvard'
     }
-];//.then(({ name, id, email, school}) => {
-//     const intern = new Intern(name, id, email, school);
-//     teamArray.push(intern);
-//     console.log(teamArray);
-// });
+];
 
-//functions go here 
-const confirmEmp = ({ confirmEmp }) => {
+//functions go here
+// const chooseEmployeeType = ( {type} ) => {
+//     let response;
+//     switch(type) {
+//         case 'Engineer':{
+//             response = prompt(engineerQuestions);
+//         }
+//         case 'Intern':{
+//             response = prompt(internQuestions);
+//         }
+//         case 'Manager':{
+//             response = prompt(managerQuestions);
+//         }
+//     }
+//     console.log('response');
+// };
+
+const confirmEmp = () => {
     return prompt({
         message: 'Would you like to add more employees?',
         type: 'confirm',
@@ -99,9 +104,8 @@ const confirmEmp = ({ confirmEmp }) => {
 };
 
 const addMoreEmp = ({ addMore }) => {
-    if(addMore) {
-        console.log('Continue');
-        //need more code here 
+    if (addMore) {
+        //console.log('Continue');
         chooseEmpType()
         .then(getEmpType)
         .then(confirmEmp)
@@ -109,13 +113,13 @@ const addMoreEmp = ({ addMore }) => {
     } else {
         const template = pageTemplate(teamArray);
         fs.writeFileSync('./dist/team.html', template);
-        console.log('Success, written to File');
+        //console.log('Success, written to File');
     }
 };
 
 
 // already have this function on line 14?
-const chooseEmpType = ({ chooseType }) => {
+const chooseEmpType = () => {
     return prompt({
         type: 'rawList',
         message: 'What kind of employee would you like to add?',
@@ -129,12 +133,12 @@ const chooseEmpType = ({ chooseType }) => {
 };
 
 // already have this function on line 14?
-const getEmpType = async ({ getType }) => {
+const getEmpType = async ({ type }) => {
     switch(type) {
         case 'Engineer':{
             //return prompt(engineerQuestions);
             const response = await prompt(engineerQuestions);
-            const { name, id, email, officeNumber} = response;
+            const { name, id, email, gitHub } = response;
             const engineer = new Engineer(name, id, email, gitHub);
             teamArray.push(engineer);
             console.log(teamArray);
@@ -143,7 +147,7 @@ const getEmpType = async ({ getType }) => {
         case 'Intern':{
             //return prompt(internQuestions);
             const response = await prompt(internQuestions);
-            const { name, id, email, school} = response;
+            const { name, id, email, school } = response;
             const intern = new Intern(name, id, email, school);
             teamArray.push(intern);
             console.log(teamArray);
@@ -152,7 +156,7 @@ const getEmpType = async ({ getType }) => {
         case 'Manager':{
             //return prompt(managerQuestions);
             const response = await prompt(managerQuestions);
-            const { name, id, email, officeNumber} = response;
+            const { name, id, email, officeNumber } = response;
             const manager = new Manager(name, id, email, officeNumber);
             teamArray.push(manager);
             console.log(teamArray);
